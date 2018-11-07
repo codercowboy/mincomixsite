16f5aa62f3bf2c50aef70f39e7de03c166e05ee7
Jason Baker <jason@onejasonforsale.com>
2018-11-06 18:10:24 -0700

    Clean up html styling.

--- index.php
+++ index.php
@@ -3,24 +3,32 @@
 <head>
 <title>drawing salt</title>
 <style>
-	* { margin:0; padding:0; }
+	* { margin:0; padding:0; -webkit-text-size-adjust:100%; font-family:Arial, Verdana, serif; }
 	BODY { text-align:center; margin-bottom:30px; }
 	IMG { max-width:800px; }
-	H1 { font-size:x-large; margin-top:10px; }
-	H2 { font-size:x-small; margin-bottom:20px; }
-	.links { display:block; margin-top:20px; }
+	.pageHeader { margin-left:auto; margin-right:auto; text-align:center; margin-bottom:30px; }
+	.pageHeader H1 { font-size:6em; margin-top:10px; }
+	.pageHeader H2 { font-size:2em; margin-bottom:20px; margin-top:10px; }
+	.links { display:block; margin-top:30px; }
+	.links A { width:75px; height:50px; background-color:black; color:white; display:inline-block;
+		border-color:gray; text-decoration:none; margin:25px; font-size:2em; 
+		font-weight:bold; padding-top:0.5em; }
+	.links A:hover { color:gray; }
 </style>
 </head>
 <body>
-	<h1>drawing salt</h1>
-	<h2>comics for people having days</h2>	
+	<div class="pageHeader">
+		<h1>drawing salt</h1>
+		<h2>comics for people having days</h2>	
+	</div>
 
 	<img src="<?= $currentComicImage ?>"/>
+
 	<div class="links">
-		<a href="?comic=first">first</a>
-		<a href="?comic=<?= $previousComicDate ?>">previous</a>
-		<a href="?comic=<?= $nextComicDate ?>">next</a>
-		<a href="?comic=last">last</a>
+		<a href="?comic=first">&lt;&nbsp;&lt;</a>
+		<a href="?comic=<?= $previousComicDate ?>">&lt;</a>
+		<a href="?comic=<?= $nextComicDate ?>">&gt;</a>
+		<a href="?comic=last">&gt;&nbsp;&gt;</a>
 	</div>
 </body>
 
